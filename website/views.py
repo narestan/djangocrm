19f36920ddd0558b35260f968cb3a3b70ec80bc1
iman <narestan.im@gmail.com>
2023-09-15 10:24:16 +0330

    customers

--- website/views.py
+++ website/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.csrf import csrf_protect
-from django.views.decorators.cache import cache_page
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -35,5 +34,19 @@ def logout_user(request):
 	return redirect('home')
 
 def register(request):
-    return render(request, 'register.html', {})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
 
+	return render(request, 'register.html', {'form':form})
